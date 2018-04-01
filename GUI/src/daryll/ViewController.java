2723ddf4bacb32a2ce997bb747cc3ec59166befd
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-04-01 15:45:34 +0200

    Debut of the GUI, first implemeting, need some update later

---
+++ GUI/src/daryll/ViewController.java
@@ -0,0 +1,21 @@
+package daryll;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Labinot
+ */
+public class ViewController implements Initializable {
+   
+   @Override
+   public void initialize(URL url, ResourceBundle rb) {
+      // TODO
+   }   
+   
+}
