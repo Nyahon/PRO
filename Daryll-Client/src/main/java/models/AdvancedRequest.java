ee0b84702611b516ba540e986b8f7674b77b7f70
Ludwig9392 <loic.frueh@gmail.com>
2018-05-19 19:42:58 +0200

    Update AdvancedRequest in order to simplify the life of my skillfull mates

--- Daryll-Client/src/main/java/models/AdvancedRequest.java
+++ Daryll-Client/src/main/java/models/AdvancedRequest.java
@@ -1,17 +1,31 @@
 package models;
 
+import Utils.PeriodManager;
+
 import java.sql.Date;
 
 public class AdvancedRequest {
     private String building;
     private Date date;
     private int idPeriodBegin;
-    private int idPeriodEnd;
+    private int idPeriodEnd = PeriodManager.PERIODS_END.size();
     private String floor;
-    private ClassRoom classroom;
+    private String classroom;
+
+
+
+    public AdvancedRequest(String building, Date date, int idPeriodBegin,
+                           String floor, String classroom) {
+
+        this.building = building;
+        this.date = date;
+        this.idPeriodBegin = idPeriodBegin;
+        this.floor = floor;
+        this.classroom = classroom;
+    }
 
     public AdvancedRequest(String building, Date date, int idPeriodBegin, int idPeriodEnd,
-                           String floor, ClassRoom classroom) {
+                           String floor, String classroom) {
 
         this.building = building;
         this.date = date;
@@ -62,10 +76,10 @@ public class AdvancedRequest {
     }
 
     public String getClassroom() {
-        return classroom.getClassRoom();
+        return classroom;
     }
 
-    public void setClassroom(ClassRoom classroom) {
-        this.classroom = new ClassRoom(classroom.getClassRoom());
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
     }
 }
