02f115d3f5e1ac7dccef6127040adbc4b142e258
Ludwig9392 <loic.frueh@gmail.com>
2018-05-19 21:41:45 +0200

    Fixed a f****** bug in ClassRoom

--- Daryll-Client/src/main/java/models/TimeSlot.java
+++ Daryll-Client/src/main/java/models/TimeSlot.java
@@ -4,7 +4,7 @@ import java.sql.Date;
 
 public class TimeSlot {
 
-    private ClassRoom classroom = ClassRoom.NO_CLASSROOM;
+    private ClassRoom classroom= new ClassRoom();
     private Date date;
     private int idPeriod;
 
