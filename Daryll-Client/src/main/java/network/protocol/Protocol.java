95d49ad5f127bac79e55a5ffd6bacdbf32bd9093
romaingallay <romain.gallay@heig-vd.ch>
2018-05-19 13:13:51 +0200

    add missing a command on protocol

---
+++ Daryll-Client/src/main/java/network/protocol/Protocol.java
@@ -0,0 +1,32 @@
+package network.protocol;
+
+/**
+ * This class defines constants for the Roulette Protocol (version 2)
+ *
+ * @author Olivier Liechti
+ */
+public class Protocol {
+
+    public final static String VERSION = "1.0";
+
+    public final static int DEFAULT_PORT = 2613;
+    public final static String SERVER_IP = "locahost";
+
+    public final static String CMD_CLASSROOM = "CLASSROOM";
+    public final static String CMD_TIMESLOT = "TIMESLOT";
+    public final static String CMD_FLOOR = "FLOOR";
+    public final static String CMD_BYE = "BYE";
+
+    public final static String RESPONSE_OK = "OK";
+    public final static String RESPONSE_CONNECTED = "CONNECTED";
+
+    public final static String RESPONSE_CLASSROOM = "READY";
+    public final static String RESPONSE_TIMESLOT = "READY";
+    public final static String RESPONSE_FLOOR = "READY";
+    public final static String RESPONSE_BYE = "BYE_BYE";
+
+
+
+    public final static String[] SUPPORTED_COMMANDS = new String[]{CMD_CLASSROOM, CMD_TIMESLOT};
+
+}
