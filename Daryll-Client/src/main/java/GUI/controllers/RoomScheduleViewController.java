b69d1094e7d9a48aef1af0eefa2f1a480f9339b2
manalito <aurelsiu@hotmail.com>
2018-05-21 13:27:58 +0200

    update RoomSchedule functions

--- Daryll-Client/src/main/java/GUI/controllers/RoomScheduleViewController.java
+++ Daryll-Client/src/main/java/GUI/controllers/RoomScheduleViewController.java
@@ -12,9 +12,19 @@
  */
 package GUI.controllers;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import models.ClassRoom;
+import models.TimeSlot;
+
+import static controller.Controller.handleClientClassroomRequest;
 
 /**
  * FXML Controller class
@@ -22,11 +32,26 @@ import javafx.fxml.Initializable;
  * @author Labinot
  */
 public class RoomScheduleViewController implements Initializable {
-   
+
+   @FXML
+   private TextField scheduleRoomTextField;
+
+   String classroomRequested = "";
+
    /**
     * Handler that will give the schedule of the room
     */
    public void giveRoomSchedule () {
+
+
+      ClassRoom classRoomRequested = new ClassRoom(scheduleRoomTextField.getText());
+
+      try {
+         List<TimeSlot> listClass = handleClientClassroomRequest(classRoomRequested);
+      } catch (IOException e){
+         e.printStackTrace();
+      }
+
       
    }
    
