304a0e9c01b0eea7dbe9eae366126a00ffc17187
nyahon <yohann.meyer@gmail.com>
2018-05-23 17:34:35 +0200

    is this reality ? is this just fantasy ?

--- server/src/main/java/utils/config.java
+++ server/src/main/java/utils/config.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class config {
-    public static final boolean isInPRODUCTION = false;
+    public static final boolean isInPRODUCTION = true;
 
     public static final String prodIP = "172.18.0.42";
     public static final String devIP = "localhost";
